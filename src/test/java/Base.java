4fdd68c16258305d7d83e60dfa02b7cfd2e1e037
Pavlovic Bojan <bojanpavlovicoffice@gmail.com>
2024-03-26 09:58:38 +0100

    Create Base.java

---
+++ src/test/java/Base.java
@@ -0,0 +1,11 @@
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class Base {
+
+   @BeforeClass
+    public static void setUp() {
+        RestAssured.baseURI = "http://localhost:8080/";
+    }
+
+}
